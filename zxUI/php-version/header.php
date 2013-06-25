dde4f07e9a2d7b3a269b937345e57d61e42a57e3
zhongxing <king.521.love@gmail.com>
2013-06-26 07:21:30 +0800

    'update'

--- zxUI/php-version/header.php
+++ zxUI/php-version/header.php
@@ -56,12 +56,12 @@
 	<style type="text/css">
 	
 /* nav */
-#nav{position:relative;width:950px;height:60px;margin:5px auto 15px auto;}
-
+#nav{position:relative;width:950px;height:34px;margin:5px auto 30px 20px;background-color:#43a1da;}
+#nav ul{margin:0 0 9px 1px}
 #nav li{float:left;list-style:none;text-align:center;font-size:13px;}
 #nav li .v a{height:14px;line-height:14px;display:block;color:#FFF;float:left;font-family:Arial,Verdana,Tahoma,"宋体";}
 #nav li .v a:hover,#nav li .v .sele{ text-decoration:none;color:yellow;height:14px;line-height:14px;font-size:15px;padding-left:0px;}
-#nav .kind_menu{height:15px;*height:16px;line-height:20px;vertical-align:middle;position:absolute;left:70px;width:880px;text-align:left;display:none;color:#000;font-size:12px;}
+#nav .kind_menu{height:15px;*height:16px;line-height:20px;vertical-align:middle;position:absolute;left:5px;width:880px;text-align:left;display:none;color:#000;font-size:12px;}
 #nav .kind_menu a{color:#000;float:left;text-align:center;width:60px;font-family:Arial,Verdana,Tahoma,"宋体";font-size:10px;}
 #nav .kind_menu a:hover{color:#000;border-bottom:2px #a60000 solid;}
 #nav .kind_menu span{font-size:15px;color:#000;line-height:30px;*line-height:26px;float:left }	
@@ -85,7 +85,7 @@
 			<!--- end top view -->
 			<!-- user dropdown starts -->
 			<!---not login --->
-					  <div class="pull-right span4 show-grid">
+					  <div class="pull-right span4">
 					  	<a href="#" class="pull-right"><button class="btn btn-mini">分享</button></a>
 						<a href="#" class="pull-right"><button class="btn btn-mini">登录</button></a>
 						<a href="#" class="pull-right"><button class="btn btn-mini">免费注册</button></a>
@@ -110,7 +110,6 @@
 					</ul>
 					<!--- end login --->
 				</div>
-				<!-- user dropdown ends -->
 		</div>
 	</div>
 <!-- end navbar -->
@@ -145,18 +144,18 @@
 		<div id="nav">
 		
 		
-		<div class="l"></div>
+		
 		<ul class="c">
-		<p class="btn-group">
+		
 			<li class="nav_lishw" id="">
 				<button class="btn btn-large btn-primary"><span class="v"><a class="white" href="http://www.17sucai.com/" target="_blank" class="sele">首页</a></span> </button>
-				<div class="kind_menu" style="left: 50px; display: block;">首页二级分类</div>
+				<div class="kind_menu" style=" display: block;">首页二级分类</div>
 			</li>
 		
 			
 			<li class="" id="">
 				<button class="btn btn-large btn-primary"><span class="v"> <a class="whiete" href="http://www.17sucai.com/" class="">我要投资</a></span> </button>
-				<div class="kind_menu" style="left: 40px; display: none;">
+				<div class="kind_menu" style="display: none;">
 				<p class="btn-group">
 					<button class="btn "><a href="http://www.17sucai.com/">jquery</a></button>
 					<button class="btn "><a href="http://www.17sucai.com/">jquery</a></button>
@@ -170,7 +169,7 @@
 			</li>
 			<li class="" id="">
 				<span class="v"><button class="btn btn-large btn-primary"> <a class="whiete" href="http://www.17sucai.com/" class="">我要投资</a></button></span> 
-				<div class="kind_menu" style="left: 40px; display: none;">
+				<div class="kind_menu" style=" display: none;">
 				<p class="btn-group">
 					<button class="btn"><a href="http://www.17sucai.com/">jquery</a></button>
 					<button class="btn"><a href="http://www.17sucai.com/">jquery</a></button>
@@ -184,7 +183,7 @@
 			</li>
 			<li class="" id="">
 				<span class="v"><button class="btn btn-large btn-primary"> <a class="whiete" href="http://www.17sucai.com/" class="">我要投资</a></button></span> 
-				<div class="kind_menu" style="left: 40px; display: none;">
+				<div class="kind_menu" style=" display: none;">
 				<p class="btn-group">
 					<button class="btn"><a href="http://www.17sucai.com/">jquery</a></button>
 					<button class="btn"><a href="http://www.17sucai.com/">jquery</a></button>
@@ -198,7 +197,7 @@
 			</li>
 			<li class="" id="">
 				<span class="v"><button class="btn btn-large btn-primary"> <a class="whiete" href="http://www.17sucai.com/" class="">我要投资</a></button></span> 
-				<div class="kind_menu" style="left: 40px; display: none;">
+				<div class="kind_menu" style=" display: none;">
 				<p class="btn-group">
 					<button class="btn"><a href="http://www.17sucai.com/">jquery</a></button>
 					<button class="btn"><a href="http://www.17sucai.com/">jquery</a></button>
@@ -212,7 +211,7 @@
 			</li>
 			<li class="" id="">
 				<span class="v"><button class="btn btn-large btn-primary"> <a class="whiete" href="http://www.17sucai.com/" class="">我要投资</a></button></span> 
-				<div class="kind_menu" style="left: 40px; display: none;">
+				<div class="kind_menu" style=" display: none;">
 				<p class="btn-group">
 					<button class="btn"><a href="http://www.17sucai.com/">jquery</a></button>
 					<button class="btn"><a href="http://www.17sucai.com/">jquery</a></button>
@@ -226,7 +225,7 @@
 			</li>
 			<li class="" id="">
 				<span class="v"><button class="btn btn-large btn-primary"> <a class="whiete" href="http://www.17sucai.com/" class="">我要投资</a></button></span> 
-				<div class="kind_menu" style="left: 40px; display: none;">
+				<div class="kind_menu" style=" display: none;">
 				<p class="btn-group">
 					<button class="btn"><a href="http://www.17sucai.com/">jquery</a></button>
 					<button class="btn"><a href="http://www.17sucai.com/">jquery</a></button>
@@ -238,7 +237,7 @@
 					</p>
 				</div>
 			</li>
-			</p>
+			
 		</ul>
 		
 	</div>
